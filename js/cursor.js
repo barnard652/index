0422f44b92b6d57bb415436c7e6bd27e79ffd849
barnard652 <47794490+barnard652@users.noreply.github.com>
2020-11-05 20:22:22 +0000

    Update cursor.js

--- js/cursor.js
+++ js/cursor.js
@@ -1,23 +1,13 @@
 // function to change cursor - designe for new website 2020
 
-	const cursor = document.querySelector('.cursor');
-
-        document.addEventListener('mousemove', e => {
-            cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
-        })
-
-        document.addEventListener('click', () => {
-            cursor.classList.add("click");
-
-            setTimeout(() => {
-                cursor.classList.remove("click");
-            }, 500)
-        })
-		
-		document.addEventListener('scroll', () => {
-            cursor.classList.add("scroll");
-
-            setTimeout(() => {
-                cursor.classList.remove("scroll");
-            }, 1000)
-        })
+document.addEventListener("mousemove", function (event) {
+	const x = event.pageX
+	const y = event.pageY
+	
+	const midX = x - window.innerWidth / 2
+	const midY = y - window.innerWidth / 2
+	
+	const box = document.querySelector(".cursor")	
+	box.style.left = x + "px"
+	box.style.top = y + "px"
+})
