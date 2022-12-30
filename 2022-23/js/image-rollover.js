3e96e0255785ee01f162af579f8945a30dd9e96c
barnard652 <47794490+barnard652@users.noreply.github.com>
2022-12-30 16:39:33 +0000

    Create image-rollover.js

---
+++ 2022-23/js/image-rollover.js
@@ -0,0 +1,50 @@
+// image rollover -->
+
+  document.getElementById("graphic").addEventListener("mouseover", function() {
+    document.getElementById("BG").style.backgroundImage = "url(https://66.media.tumblr.com/75fb4140d41cc9e77c24dd6e4f4dc245/tumblr_p3lju0tpj91tm7xnjo1_r1_1280.jpg)";
+  }, false);
+  document.getElementById("graphic").addEventListener("mouseout", function() {
+     document.getElementById("BG").style.backgroundImage = "";
+  }, false);
+
+  document.getElementById("branding").addEventListener("mouseover", function() {
+    document.getElementById("BG").style.backgroundImage = "url(https://mir-s3-cdn-cf.behance.net/project_modules/fs/c6841f100072951.5f00c23593612.jpg";
+  }, false);
+  document.getElementById("branding").addEventListener("mouseout", function() {
+     document.getElementById("BG").style.backgroundImage = "";
+  }, false);
+  
+  document.getElementById("website").addEventListener("mouseover", function() {
+    document.getElementById("BG").style.backgroundImage = "url(https://github.com/barnard652/index/blob/master/img/stop-writing-shit-code/stop-writting-shit-code.png?raw=true)";
+  }, false);
+  document.getElementById("website").addEventListener("mouseout", function() {
+     document.getElementById("BG").style.backgroundImage = "";
+  }, false);
+  
+  document.getElementById("UI/UX").addEventListener("mouseover", function() {
+    document.getElementById("BG").style.backgroundImage = "url(https://github.com/barnard652/index/blob/master/img/spark/falling-business-card-mockups.jpg?raw=true)";
+  }, false);
+  document.getElementById("UI/UX").addEventListener("mouseout", function() {
+     document.getElementById("BG").style.backgroundImage = "";
+  }, false);
+  
+  document.getElementById("motion").addEventListener("mouseover", function() {
+    document.getElementById("BG").style.backgroundImage = "url(https://github.com/barnard652/index/blob/master/img/Comp%203.gif?raw=true)";
+  }, false);
+  document.getElementById("motion").addEventListener("mouseout", function() {
+     document.getElementById("BG").style.backgroundImage = "";
+  }, false);
+  
+  document.getElementById("illustration").addEventListener("mouseover", function() {
+    document.getElementById("BG").style.backgroundImage = "url(https://github.com/barnard652/index/blob/master/img/Illustation-Image.png?raw=true)";
+  }, false);
+  document.getElementById("illustration").addEventListener("mouseout", function() {
+     document.getElementById("BG").style.backgroundImage = "";
+  }, false);
+
+  document.getElementById("stuff").addEventListener("mouseover", function() {
+    document.getElementById("BG").style.backgroundImage = "url(https://media3.giphy.com/media/1YaYwvueDztl6nWXyW/giphy.gif?cid=790b7611a7858ef6cb25fade3fcafe172c4bb07b13e02abb&rid=giphy.gif&ct=g)";
+  }, false);
+  document.getElementById("stuff").addEventListener("mouseout", function() {
+     document.getElementById("BG").style.backgroundImage = "";
+  }, false);
