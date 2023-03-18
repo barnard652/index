bd03b5f1563fd22c912dd7c7b3eb964a839a9d7a
barnard652 <47794490+barnard652@users.noreply.github.com>
2023-03-18 19:48:37 +0000

    Update cursor.js

--- 2022-23/js/cursor.js
+++ 2022-23/js/cursor.js
@@ -9,9 +9,9 @@ let aimY = 0
 
 balls.forEach((ball, index) => {
   let currentX = 0
-	let currentY = 0
+  let currentY = 0
 
-	let speed = 0.3 - index * 0.015
+  let speed = 0.3 - index * 0.015
 
   const animate = function () {
     currentX += (aimX - currentX) * speed
@@ -20,20 +20,27 @@ balls.forEach((ball, index) => {
     ball.style.left = currentX + "px"
     ball.style.top = currentY + "px"
 
+    // Check distance from right edge of the window
+    const windowWidth = window.innerWidth || document.documentElement.clientWidth
+    const distanceFromRight = windowWidth - currentX
+    if (distanceFromRight < 350) {
+      cursorTag.classList.add("left")
+    } else {
+      cursorTag.classList.remove("left")
+    }
+
     requestAnimationFrame(animate)
   }
 
   animate()
 })
 
-
-
 document.addEventListener("mousemove", function (event) {
   aimX = event.pageX
   aimY = event.pageY
 })
 
-images.forEach(image => {
+images.forEach((image) => {
   image.addEventListener("mouseover", function () {
     ballMessage.classList.add("visible")
     ballMessage.innerHTML = image.getAttribute("data-hover")
